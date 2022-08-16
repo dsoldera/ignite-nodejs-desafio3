633b667e90b1f10a2c2b6b34b5b5f11d63e3bb7c
dsoldera <dsoldera@gmail.com>
2022-08-16 19:52:13 -0300

    fix desafio 3

---
+++ README.md
@@ -0,0 +1,13 @@
+# Desafio 3
+
+Instruções https://www.notion.so/Desafio-03-Corrigindo-o-c-digo-c15c8a2e212846039a367cc7b763c6dd
+
+Descrição de cada propriedade:
+
+- **id** deve ser um uuid válido;
+- **title** é o título do repositório (por exemplo "unform");
+- **url** é a URL que aponta para o repositório (por exemplo "[https://github.com/unform/unform](https://github.com/unform/unform)");
+- **techs** é um array onde cada elemento deve ser uma string com o nome de uma tecnologia relacionada ao repositório (por exemplo: ["react", "react-native", "form"]);
+- **likes** é a quantidade de likes que o repositório recebeu (e que vai ser incrementada de 1 em 1 a cada chamada na rota de likes).
+
+Note que a quantidade de likes deve sempre ser zero no momento de criação.
--- src/index.js
+++ src/index.js
@@ -23,32 +23,34 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryId = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repositoryId) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { ...repositories[repositoryId], ...updatedRepository };
 
-  repositories[repositoryIndex] = repository;
+  repositories[repositoryId] = repository;
 
-  return response.json(repository);
+  return response.status(204).json();
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.indexOf(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex === -1) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
